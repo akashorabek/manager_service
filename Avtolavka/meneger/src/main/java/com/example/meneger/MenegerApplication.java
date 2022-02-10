07a88cade1cba3de0dc3b41d47a157c9cb7b3ea1
Anita Tkachenko <anitaguskova3@gmail.com>
2022-02-10 11:32:20 +0600

    #1 Добавлены файлы

---
+++ Avtolavka/meneger/src/main/java/com/example/meneger/MenegerApplication.java
@@ -0,0 +1,13 @@
+package com.example.meneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenegerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenegerApplication.class, args);
+    }
+
+}
