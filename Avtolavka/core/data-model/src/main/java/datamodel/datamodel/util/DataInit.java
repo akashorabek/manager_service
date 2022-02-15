c01741f48308c50679bd3ae8b4be6943da51dbf2
Dmitriy Pak <dmt.alfa@gmail.com>
2022-02-15 14:25:12 +0600

    #9  Добавлена загрузка тестовых записей в таблицу clients.

--- Avtolavka/core/data-model/src/main/java/datamodel/datamodel/util/DataInit.java
+++ Avtolavka/core/data-model/src/main/java/datamodel/datamodel/util/DataInit.java
@@ -1,5 +1,6 @@
 package datamodel.datamodel.util;
 
+import datamodel.datamodel.model.Client;
 import datamodel.datamodel.model.ClientStatus;
 import datamodel.datamodel.repository.ClientRepository;
 import datamodel.datamodel.repository.ClientStatusRepository;
@@ -8,7 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.UUID;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Configuration
@@ -21,6 +22,7 @@ public class DataInit {
     public CommandLineRunner init() {
         try {
             initClientStatuses().run();
+            initClients().run();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -37,6 +39,32 @@ public class DataInit {
         return new ClientStatus[]{
                 new ClientStatus(1, "Новый"),
                 new ClientStatus(2, "Постоянный")
+        };
+    }
+
+    private CommandLineRunner initClients() {
+        return (args) -> Stream.of(clients())
+                .peek(System.out::println)
+                .forEach(clientRepository::save);
+    }
+
+    private Client[] clients() {
+        List<ClientStatus> statuses = clientStatusRepository.findAll();
+        return new Client[]{
+                new Client(1,
+                        "Тест клиент новый",
+                        "ск ном 1",
+                        "Almaty",
+                        "777",
+                        "test1@gmail.com",
+                        statuses.get(0)),
+                new Client(2,
+                        "Тест клиент постоянный",
+                        "ск ном 2",
+                        "Boston",
+                        "555",
+                        "test2@gmail.com",
+                        statuses.get(1))
         };
     }
 }
