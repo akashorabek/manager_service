c88cbc8158cc2b97546940aeadf7c5f088893537
Madi <pinkykity16@gmail.com>
2022-02-16 18:03:10 +0600

    Убрал аннотацию @Data у классов

--- Avtolavka/core/data-model/src/main/java/kz/attractor/datamodel/model/ClientStatus.java
+++ Avtolavka/core/data-model/src/main/java/kz/attractor/datamodel/model/ClientStatus.java
@@ -1,12 +1,10 @@
 package kz.attractor.datamodel.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Data
 @Entity
 @Table(name = "clients_statuses")
 @NoArgsConstructor
