dd9d3c905e8fc0efed4c34f2397525ba93ca7fab
Madi <pinkykity16@gmail.com>
2022-02-17 19:26:19 +0600

    Тикет №5 - удалил не нужный sql файл и сделал наполнение товаров в DataInit(добавил 5 товаров)

--- Avtolavka/application/api/src/main/resources/db/migration/V1000__insert_products.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO products(id, in_stock, name, purchase_price, quantity, selling_price) VALUES (1, true, 'ВТУЛКА КОЛЕНВАЛА', 3312, 10, 5378.7);
-INSERT INTO products(id, in_stock, name, purchase_price, quantity, selling_price) VALUES (2, true, 'Втулка коленчатого вала без шпон. паза 406.1005038-11 100504', 1580, 10, 2565.92);
-INSERT INTO products(id, in_stock, name, purchase_price, quantity, selling_price) VALUES (3, true, 'Адаптер для дрели М14', 406, 10, 659.34);
-INSERT INTO products(id, in_stock, name, purchase_price, quantity, selling_price) VALUES (4, true, 'Масло Газпромнефть Супер 10W40 SG/CD 5л.', 4679, 10, 7598.7);
-INSERT INTO products(id, in_stock, name, purchase_price, quantity, selling_price) VALUES (5, true, 'ET-912-YE', 173, 10, 280.95);
