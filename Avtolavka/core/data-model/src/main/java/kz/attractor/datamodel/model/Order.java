95c946cf714c395e62a6bb22e94920b3ae348222
Dmitriy Pak <dmt.alfa@gmail.com>
2022-02-19 14:52:16 +0600

    #10 Добавлены сущности КП и сущности для хранения таблицы orders_products для хранения продуктов, относящихся к какому-то заказу. В BD добавлены демо записи.

---
+++ Avtolavka/core/data-model/src/main/java/kz/attractor/datamodel/model/Order.java
@@ -0,0 +1,26 @@
+package kz.attractor.datamodel.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date_create")
+    private LocalDateTime dateCreation;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+}
