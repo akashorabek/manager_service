c3a1212559382799bd201443f1092ea605a20336
Dmitriy Pak <dmt.alfa@gmail.com>
2022-02-19 21:23:52 +0600

    #10 Добавлены контроллер, сервис, dto для вывода сущности КП. Добавлены методы для получения списка КП и создания в базе нового КП

---
+++ Avtolavka/application/api/src/main/java/kz/attractor/api/controller/OrderController.java
@@ -0,0 +1,23 @@
+package kz.attractor.api.controller;
+
+import kz.attractor.api.dto.OrderDto;
+import kz.attractor.api.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping("/orders")
+    public String showOrdersPage(Model model) {
+        List<OrderDto> orders = orderService.findAll();
+        model.addAttribute("orders", orders);
+        return "orders";
+    }
+}
