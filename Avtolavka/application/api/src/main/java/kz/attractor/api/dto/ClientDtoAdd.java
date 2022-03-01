f1e668010fc2ac6496fcc76e077f4b8238d9502b
Dmitriy Pak <dmt.alfa@gmail.com>
2022-03-01 21:16:05 +0600

    #43 Добавлена валидация на поле банка. При создании клиента это поле не должно быть пустым

--- Avtolavka/application/api/src/main/java/kz/attractor/api/dto/ClientDtoAdd.java
+++ Avtolavka/application/api/src/main/java/kz/attractor/api/dto/ClientDtoAdd.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -26,5 +27,6 @@ public class ClientDtoAdd {
     @Email(message = "В поле должен находиться email")
     private String email;
 
+    @NotNull(message = "Поле не должно быть пустым")
     private ClientBank bank;
 }
