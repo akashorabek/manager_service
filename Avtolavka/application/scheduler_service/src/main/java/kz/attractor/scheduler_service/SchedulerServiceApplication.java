156c6abec9807c5ef395c4e82d543bfc7cf9f7a1
Anita Tkachenko <anitaguskova3@gmail.com>
2022-03-01 21:01:50 +0600

    #21 Создан модуль Scheduler_service

---
+++ Avtolavka/application/scheduler_service/src/main/java/kz/attractor/scheduler_service/SchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.attractor.scheduler_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerServiceApplication.class, args);
+    }
+
+}
